61ec974b2094b446be378d8bc00a5e89454f915a
klunceford743 <kml039@bucknell.edu>
2016-11-06 23:37:56 -0500

    Add files via upload

---
+++ tree.py
@@ -0,0 +1,22 @@
+""" Katie Lunceford
+"""
+
+class Node:
+
+    def __init__(self, numChildren):
+        self.data = None
+        self.child1 = None
+        self.child2 = None
+        self.child3 = None
+        self.child4 = None
+        self.child5 = None
+        self.child6 = None
+
+
+class DecisionTree:
+
+    def __init__(self):
+        self.root = None
+
+    def train(list1, list2, maxDepth):
+        
