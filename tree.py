27c09bc3172e8e66f3bdd66ce278bdb3f197f687
klunceford743 <kml039@bucknell.edu>
2016-11-08 15:20:02 -0500

    Add files via upload

--- tree.py
+++ tree.py
@@ -3,20 +3,58 @@
 
 class Node:
 
-    def __init__(self, numChildren):
-        self.data = None
+    def __init__(self, data):
+        self.data = data
         self.child1 = None
         self.child2 = None
         self.child3 = None
         self.child4 = None
-        self.child5 = None
-        self.child6 = None
-
 
 class DecisionTree:
 
     def __init__(self):
         self.root = None
 
-    def train(list1, list2, maxDepth):
-        
+    def train(self, data, inputAttr, maxDepth):
+        if list1 == []:
+            raise Exception
+        state = True
+        val = data[0][0]
+        for x in range(len(data)):
+            if data[x][0] != val:
+                state = False
+                break
+        if state:
+            return Node(val)
+        if len(inputAttr) == 1:
+            items = []
+            for x in range(len(data)):
+                items.append(data[x][0])
+            val = mostFreq(items)
+            return Node(val)
+
+
+
+
+
+def mostFreq(items):
+    dic = {}
+    for i in range(len(items)):
+        if items[i] in dic:
+            dic[items[i]] += 1
+        else:
+            dic[items[i]] = 1
+    maximum = max(dic, key=dic.get)
+    return maximum
+
+def maxGain(data):
+    entropy = 1
+    for x in range(len(data[0])):
+        items = []
+        for y in range(len(data)):
+            items.append(data[y][x])
+        ent = calculateEntropy(items)
+
+def calculateEntropy(items):
+    
+
