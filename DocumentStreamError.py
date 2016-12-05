2c7fb7cadb8d14f46f1f8cdc2c08f7ca8f4bcfc3
klunceford743 <kml039@bucknell.edu>
2016-12-05 03:44:56 -0500

    Add files via upload

--- DocumentStreamError.py
+++ DocumentStreamError.py
@@ -1,4 +1,4 @@
-"""Katie Lunceford
+"""Katie Lunceford and Jon Li
 
 This is the DocumentStreamError object that is an exception used to catch any
 errors that appear while using the DocumentStreamObject.
