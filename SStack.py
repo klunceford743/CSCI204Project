5e90f4858e75f9c1166f63aa6f2b17089dd53f95
klunceford743 <kml039@bucknell.edu>
2016-12-05 03:53:07 -0500

    Add files via upload

--- SStack.py
+++ SStack.py
@@ -1,4 +1,5 @@
-""" Katie Lunceford
+""" Katie Lunceford//Jon Li
+a simple stack using linked list to be used later on
 """
 
 import sList as link
@@ -6,14 +7,17 @@ import sList as link
 class SStack:
 
     def __init__(self):
+        '''establishes the linked list'''
         self.st = link.SLink()
         self.size = 0
 
     def push(self, val):
+        '''pushes the val into the stack'''
         self.st.insert(val)
         self.size += 1
 
     def pop(self):
+        '''removes the top of the stack and returns the value of that'''
         if self.st.head is None:
             return None
         val = self.st.head.data
--- Sentence.py
+++ Sentence.py
@@ -1,9 +1,8 @@
-"""Katie Lunceford
+"""Katie Lunceford//Jon Li
 This is the Sentence object. It stores a string that represents a sentence with
 punctuation. The attributes are string (the string of the sentence), wordCount
 (the number of words in the sentence), charCount (the number of characters in
 the sentence), and puncuation (the punctuation that the sentence ends with).
-
 The methods are parseWords and parseChar.
 """
 
@@ -19,11 +18,13 @@ class Sentence:
     #returns a list of all of the words in the sentence, using the split
     #method
     def parseWords(self):
+        '''parses the document's words, which means the document is prepared for other operations by being split '''
         return self.string.split()
 
     #returns a list of all of the characters in the sentence, using a loop
     #through the string
     def parseChar(self):
+        '''parses through the characters in each of the words that were parsed.'''
         characters = []
         for char in self.string:
             characters.append(char)
